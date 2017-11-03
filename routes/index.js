86bd7828c8b35940bfe209fbb440700dda93e2c6
Willi <zwj.nbsp@gmail.com>
2017-11-03 12:15:37 +0800

    project initialize

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    console.log(path.join(__dirname, '../public/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../public/dist/index.html'));
+});
+
+module.exports = router;
